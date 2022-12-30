6e33acbe9a62d0d69f035a08010e473dfd14e71d
mmednik <mmednik@gmail.com>
2022-12-30 10:51:02 -0300

    keyboard custom hook

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,8 +2,20 @@ import { useSphere } from "@react-three/cannon"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useRef } from "react"
 import { Vector3 } from 'three'
+import { useKeyboard } from "../hooks/useKeyboard"
+
+const CHARACTER_SPEED = 2
+const CHARACTER_JUMP_FORCE = 10
 
 export const Player = () => {
+  const {
+    moveBackward,
+    moveForward,
+    moveLeft,
+    moveRight,
+    jump
+  } = useKeyboard()
+
   const { camera } = useThree()
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -34,7 +46,31 @@ export const Player = () => {
       )
     )
 
-    api.velocity.set(0, 0, -1)
+    const direction = new Vector3()
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    )
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    )
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(CHARACTER_SPEED)
+      .applyEuler(camera.rotation)
+
+    api.velocity.set(
+      direction.x,
+      vel.current[1],
+      direction.z
+    )
   })
 
   return (
